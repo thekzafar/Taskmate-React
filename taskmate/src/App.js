b73b6ea139d713c1f233061911121a72934c872d
thekzafar <karimazafar.yvf@gmail.com>
2024-05-09 02:00:26 +0600

    folder 2 basic structure complete

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,11 +1,17 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Taskmate</h1>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+//Start folder 3 Video 009
---
+++ taskmate/src/components/Header.js
@@ -0,0 +1,9 @@
+import Logo from '../assets/logo.png'
+
+export const Header = () => {
+  return (
+    <div>
+        <img src={Logo} alt="" className="logo" />
+    </div>
+  )
+}
